025130f2d858caeb1adc872c1eb0b423b38738cc
heyongxian <heyongxian@kuaishou.com>
2026-03-12 11:23:58 +0800

    feat: 添加事件和调度器的编译选项允许未使用变量和代码

--- src/base/scheduler.rs
+++ src/base/scheduler.rs
@@ -1,4 +1,6 @@
-#[allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
 use crate::base::{Ref, RefPtr};
 use std::collections::HashMap;
 use std::rc::Rc;
